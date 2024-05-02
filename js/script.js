f52e21f08318fac853c53a1ff6d55ea00a3da06b
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-05-02 04:05:31 +0000

    flowchart +everythign else

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Clara All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Clara Tyman
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  // burger cost variable
+  let priceOfBurger = 5
+  // input
+  const bun = document.getElementById("seeded-bun").checked
+  const patty = document.getElementById("two-pattys").checked
+  const lettuce = document.getElementById("lettuce").checked
+  const tomato = document.getElementById("tomato").checked
+  const bacon = document.getElementById("bacon").checked
+  const ketchup = document.getElementById("ketchup").checked
+  const TAX = 0.13
+
+  if (bun == true) {
+  priceOfBurger = priceOfBurger + 0.50
+  }
+  if (patty == true) {
+    priceOfBurger = priceOfBurger + 0.50
+  }
+  if (lettuce == true) {
+    priceOfBurger = priceOfBurger + 0.25
+  }
+  if (tomato == true) {
+    priceOfBurger = priceOfBurger + 0.25
+  }
+  if (bacon == true) {
+    priceOfBurger = priceOfBurger + 0.25
+  }
+  if (ketchup == true) {
+    priceOfBurger = priceOfBurger + 0.25
+  }
+  // output end total
+  document.getElementById("page-content-answer").innerHTML = "Total + Tax:"
+  document.getElementById("answer").innerHTML = "$" + (priceOfBurger + (priceOfBurger * TAX)).toFixed(2)
+}
